77983a7ada457d338327074b0c010157b3bd9016
Taki Uddin <msiam18@gmail.com>
2022-05-12 12:45:41 +0600

    Got the idea of sending data as param or query

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,10 +1,47 @@
 const express = require("express");
 
-function firstMethod(req,res){
+function getAllPost(req,res){
     console.log("first method");
     res.send("FROM FIRST METHOD");
 }
 
+function getPostByID(req,res){
+    console.log(req.params.id);
+    res.send(req.params.id);
+
+    let departmentName = req.query.department;
+    console.log(departmentName);
+
+
+
+
+
+}
+
+function createPost(req,res){
+
+    console.log(req.query.title);
+    res.status(201).json({
+       "success" : true,
+        "data" : req.query,
+    });
+}
+
+function updatePost(req,res){
+    console.log("first method");
+    res.send("FROM FIRST METHOD");
+}
+
+function deletePost(req,res){
+    console.log("first method");
+    res.send("FROM FIRST METHOD");
+}
+
+
 module.exports = {
-    firstMethod : firstMethod
+    getAllPost : getAllPost,
+    getPostByID : getPostByID,
+    createPost : createPost,
+    updatePost : updatePost,
+    deletePost : deletePost
 }
--- routes/posts.router.js
+++ routes/posts.router.js
@@ -2,6 +2,11 @@ const express = require("express");
 const postController = require("../controllers/post.controller")
 const router = express.Router();
 
-router.get("/",postController.firstMethod);
+router.get("/",postController.getAllPost);
+router.get("/getbyid/:id",postController.getPostByID);
+router.post("/createPost",postController.createPost);
+router.put("/updatePost/:id",postController.updatePost);
+router.delete("/deletePost/:id",postController.deletePost);
+
 
 module.exports = router;
